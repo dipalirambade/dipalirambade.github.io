e3c9b8046250ae9790affdcce1771981e5adc349
dipalirambade <60155912+dipalirambade@users.noreply.github.com>
2020-05-31 15:42:09 -0700

    index 1 changes

---
+++ Templates/4/includes/banner.php
@@ -0,0 +1,23 @@
+  <header id="header">
+    <div class="header-inner">
+      <!-- Header image -->
+      <img src="assets/images/header-bg.jpg" alt="img">
+      <div class="header-overlay">
+        <div class="header-content">
+        <!-- Start header content slider -->
+        <h2 class="header-slide">MONEY
+          <span>When you needed it most</span>
+          <span>Easily get on card by one click</span>
+          <span>Instantly available </span>
+          </h2>
+        <!-- End header content slider -->  
+        <!-- Header btn area -->
+        <div class="header-btn-area">
+          <a class="knowmore-btn" href="#">KNOW MORE</a>
+          <a class="download-btn" href="#">Required Documents</a>
+
+        </div>
+      </div>
+      </div>      
+    </div>
+  </header>
---
+++ Templates/4/index.php
@@ -0,0 +1,373 @@
+<!DOCTYPE html>
+<html lang="en">
+<!-- Header include start-->
+	  <?php	include("includes/header.php");?>
+<!-- Header include end-->
+  <body>
+
+  <!-- BEGAIN PRELOADER -->
+  <div id="preloader">
+    <div class="loader">&nbsp;</div>
+  </div>
+  <!-- END PRELOADER -->    
+
+  <!-- SCROLL TOP BUTTON -->
+    <a class="scrollToTop" href="#"><i class="fa fa-chevron-up"></i></a>
+  <!-- END SCROLL TOP BUTTON -->
+
+  <!-- Start header section -->  
+  	<?php include("includes/banner.php")?>
+  <!-- End header section -->
+
+  <!-- Start menu section -->
+ 	<?php include("includes/menu.php");?>
+  <!-- End menu section -->
+
+  <!-- Start about section -->
+  <section id="about">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <!-- Start welcome area -->
+          <div class="welcome-area">
+            <div class="title-area">
+              <h2 class="tittle">Benefits  <span></span></h2>
+              <span class="tittle-line"></span>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt  labore et dolore magna aliqua. Ut enim ad minim veniamo laboris nis</p>
+            </div>
+            <div class="welcome-content">
+              <ul class="wc-table">
+                <li>
+                  <div class="single-wc-content wow fadeInUp">
+                    <span class="fa fa-users wc-icon"></span>
+                    <h4 class="wc-tittle">Benefits 1</h4>
+                    <p>At vero eos et accusamus et iusto odio in dignissimos ducimus voluptatum in delenatque </p>
+                  </div>
+                </li>
+                <li>
+                  <div class="single-wc-content wow fadeInUp">
+                    <span class="fa fa-sellsy wc-icon"></span>
+                    <h4 class="wc-tittle">Benefits 2</h4>
+                    <p>At vero eos et accusamus et iusto odio in dignissimos ducimus voluptatum in delenatque </p>
+                  </div>
+                </li>
+                <li>
+                  <div class="single-wc-content wow fadeInUp">
+                    <span class="fa fa-line-chart wc-icon"></span>
+                    <h4 class="wc-tittle">Benefits 3</h4>
+                    <p>At vero eos et accusamus et iusto odio in dignissimos ducimus voluptatum in delenatque </p>
+                  </div>
+                </li>
+                <li>
+                  <div class="single-wc-content wow fadeInUp">
+                    <span class="fa fa-bus wc-icon"></span>
+                    <h4 class="wc-tittle">Benefits 4</h4>
+                    <p>At vero eos et accusamus et iusto odio in dignissimos ducimus voluptatum in delenatque </p>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <!-- End welcome area -->
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12">
+          <div class="about-area">
+            <div class="row">
+              <div class="col-md-5 col-sm-6 col-xs-12">
+                <div class="about-left wow fadeInLeft">
+                  <img src="assets/images/about-img.jpg" alt="img">
+                  <a class="introduction-btn" href="#">About us</a>
+                </div>
+              </div>
+              <div class="col-md-7 col-sm-6 col-xs-12">
+                <div class="about-right wow fadeInRight">
+                  <div class="title-area">
+                    <h2 class="tittle">About <span>Us</span> </h2>
+                    <span class="tittle-line"></span>
+                    <p>ed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illotore itatis et quasi architecto beatae vitae dicta sunt explicabo.</p> 
+
+                    <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores e</p>
+                    <div class="about-btn-area">
+                      <a href="#" class="button button-default" data-text="KNOW MORE"><span>KNOW MORE</span></a>
+                    </div>                    
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section> 
+  <!-- End about section -->
+
+  <!-- Start call to action -->
+  <section id="call-to-action">
+    <img src="assets/images/call-to-action-bg.png" alt="img">
+    <div class="call-to-overlay">
+      <div class="container">
+        <div class="call-to-content wow fadeInUp">
+          <h2>Eye catching quote goes here</h2>
+        </div>
+      </div>
+    </div> 
+  </section>
+  <!-- End call to action -->
+
+  <!-- Start Team action -->
+ 
+  <!-- Start Team action -->
+
+  <!-- Start service section -->
+  <section id="service">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="service-area">
+            <div class="title-area">
+              <h2 class="tittle">Service we offer</h2>
+              <span class="tittle-line"></span>
+              <p>perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium totam rem aperiam, eaque ipsa quae ab illo inventore</p>
+            </div>
+            <!-- service content -->
+            <div class="service-content">
+              <ul class="service-table">
+                <li class="col-md-3 col-sm-6">
+                  <div class="single-service wow slideInUp">
+                    <span class="fa fa-edit service-icon"></span>
+                    <h4 class="service-title">Service 1</h4>
+                    <p>At vero eos et accusa mus odio in dignissimos ducimus qui anditiis the tatum</p>
+                  </div>
+                </li>
+                <li class="col-md-3 col-sm-6">
+                  <div class="single-service wow slideInUp">
+                    <span class="fa fa-sort-amount-asc service-icon"></span>
+                    <h4 class="service-title">Service 2</h4>
+                    <p>At vero eos et accusa mus odio in dignissimos ducimus qui anditiis the tatum</p>
+                  </div>
+                </li>
+                <li class="col-md-3 col-sm-6">
+                 <div class="single-service wow slideInUp">
+                    <span class="fa fa-map-o service-icon"></span>
+                    <h4 class="service-title">Service 3</h4>
+                    <p>At vero eos et accusa mus odio in dignissimos ducimus qui anditiis the tatum</p>
+                  </div>
+                </li>
+                <li class="col-md-3 col-sm-6">
+                  <div class="single-service wow slideInUp">
+                    <span class="fa fa-rocket service-icon"></span>
+                    <h4 class="service-title">Service 4</h4>
+                    <p>At vero eos et accusa mus odio in dignissimos ducimus qui anditiis the tatum</p>
+                  </div>
+                </li>
+               
+                
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End service section -->
+
+  <!-- Start Portfolio section -->
+ 
+  <!-- End Portfolio section -->
+
+  <!-- Start counter section -->
+  <section id="counter">
+    <img src="assets/images/counter-bg.jpg" alt="img">
+    <div class="counter-overlay">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <!-- Start counter area -->
+            <div class="counter-area">
+              <div class="col-md-3 col-sm-6 col-xs-6">
+                <div class="single-counter">
+                  <span class="fa fa-users"></span>
+                  <div class="counter-count">
+                    <span class="counter">1050</span>
+                    <p>Happy Clients</p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-3 col-sm-6 col-xs-6">
+                <div class="single-counter">
+                  <span class="fa fa-bus"></span>
+                  <div class="counter-count">
+                    <span class="counter">750</span>
+                    <p>Project delivery</p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-3 col-sm-6 col-xs-6">
+                <div class="single-counter">
+                  <span class="fa fa-twitter"></span>
+                  <div class="counter-count">
+                    <span class="counter">2,300</span>
+                    <p>Followers</p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-3 col-sm-6 col-xs-6">
+                <div class="single-counter">
+                  <span class="fa fa-tasks"></span>
+                  <div class="counter-count">
+                    <span class="counter">800</span>
+                    <p>Project done</p>
+                  </div>
+                </div>
+              </div>    
+            </div>
+          </div>
+        </div>
+      </div>
+    </div> 
+  </section>
+  <!-- End counter section -->
+
+  <!-- Start Pricing Table section -->
+  
+  <!-- End Pricing Table section -->
+
+  <section id="client">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="client-area">
+            <ul class="client-table">
+              <li><img src="assets/images/envato.png" alt="client logo"></li>
+              <li><img src="assets/images/google.png" alt="client logo"></li>
+              <li><img src="assets/images/amazon.png" alt="client logo"></li>
+              <li><img src="assets/images/discovery.png" alt="client logo"></li>
+              <li><img src="assets/images/envato.png" alt="client logo"></li>
+              <li><img src="assets/images/tuenti.png" alt="client logo"></li>
+              <li><img src="assets/images/envato.png" alt="client logo"></li>
+              <li><img src="assets/images/google.png" alt="client logo"></li>
+              <li><img src="assets/images/amazon.png" alt="client logo"></li>
+              <li><img src="assets/images/discovery.png" alt="client logo"></li>
+              <li><img src="assets/images/envato.png" alt="client logo"></li>
+              <li><img src="assets/images/tuenti.png" alt="client logo"></li>                    
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Start Testimonial section -->
+  <section id="testimonial">
+    <img src="assets/images/testimonial-bg.jpg" alt="img">
+    <div class="counter-overlay">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <!-- Start Testimonial area -->
+            <div class="testimonial-area">
+              <div class="title-area">
+                <h2 class="tittle">What people say about us</h2>
+                <span class="tittle-line"></span>              
+              </div>
+              <div class="testimonial-conten">
+                <!-- Start testimonial slider -->
+                <div class="testimonial-slider">
+                  <!-- single slide -->
+                  <div class="single-slide">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamcoquat. Duis aute irure d olor in reprehenderit</p>
+                    <div class="single-testimonial">
+                      <img class="testimonial-thumb" src="assets/images/testimonial-image1.png" alt="img">
+                      <p>Jonna Hike</p>
+                      <span>CEO, Themeforest</span>
+                    </div>
+                  </div>
+                  <!-- single slide -->
+                  <div class="single-slide">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamcoquat. Duis aute irure d olor in reprehenderit</p>
+                    <div class="single-testimonial">
+                      <img class="testimonial-thumb" src="assets/images/team-member2.png" alt="img">
+                      <p>Jonna Hike</p>
+                      <span>CEO, Themeforest</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div> 
+  </section>
+  <!-- End Testimonial section -->
+
+ 
+  <!-- End from blog section -->
+
+  
+  <!-- Start Contact section -->
+  <section id="contact">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4 col-sm-6 col-xs-12">
+          <div class="contact-left wow fadeInLeft">
+            <h2>Contact with us</h2>
+            <address class="single-address">
+              <h4>Postal address:</h4>
+              <p>PO Box 16122 Collins Street West Victoria 8007 Australia</p>
+            </address>
+             <address class="single-address">
+              <h4>Headquarters:</h4>
+              <p>121 King Street, Melbourne Victoria 3000 Australia</p>
+            </address>
+             <address class="single-address">
+              <h4>Phone</h4>
+              <p>Phone Number: (123) 456 7890</p>
+              <p>Fax Number: (123) 456 7890</p>
+            </address>
+             <address class="single-address">
+              <h4>E-Mail</h4>
+              <p>Support: Support@example.com</p>
+              <p>Sales: sales@example.com</p>
+            </address>
+          </div>
+        </div>
+        <div class="col-md-8 col-sm-6 col-xs-12">
+          <div class="contact-right wow fadeInRight">
+            <h2>Send a message</h2>
+            <form action="" class="contact-form">
+              <div class="form-group">                
+                <input type="text" class="form-control" placeholder="Name">
+              </div>
+              <div class="form-group">                
+                <input type="email" class="form-control" placeholder="Enter Email">
+              </div>              
+              <div class="form-group">
+                <textarea class="form-control"></textarea>
+              </div>
+              <button type="submit" data-text="SUBMIT" class="button button-default"><span>SUBMIT</span></button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Contact section -->
+  <!-- Start Google Map -->
+  <section id="google-map">
+  <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m8!1m3!1d6303.67022361714!2d144.955652!3d-37.817331!3m2!1i1024!2i768!4f13.1!4m6!3e6!4m0!4m3!3m2!1d-37.8173306!2d144.9556518!5e0!3m2!1sen!2sbd!4v1442411159706" width="100%" height="500" frameborder="0" style="border:0" allowfullscreen></iframe>  
+  </section>
+  <!-- End Google Map -->
+
+  <!-- Start Footer -->    
+  
+	<?php include("includes/footer.php"); ?>
+  <!-- End Footer -->
+
+  <!-- initialize jQuery Library --> 
+  <?php include("includes/footerJs.php"); ?>
+    
+  </body>
+</html>
